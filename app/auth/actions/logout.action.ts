00dcb24cbc513ad342424a3f93d680738080f36e
4Z4C <arturoyz2105@gmail.com>
2026-04-18 23:14:59 -0500

    addComment

--- app/auth/actions/logout.action.ts
+++ app/auth/actions/logout.action.ts
@@ -5,6 +5,8 @@ import { redirect } from "react-router";
 export async function action({ request }: Route.ActionArgs) {
   const session = await getSession(request.headers.get("Cookie"));
 
+  //Hello World
+
   return redirect("/auth/login", {
     headers: {
       "Set-Cookie": await destroySession(session),
